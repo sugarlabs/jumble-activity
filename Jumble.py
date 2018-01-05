26a47783d0ef65e0e2457d18c398b8bd958309b6
Yash Agrawal <yagrawal900@gmail.com>
2018-01-06 02:20:19 +0530

    ported to gtk+ 3

--- Jumble.py
+++ Jumble.py
@@ -20,10 +20,9 @@
 
 
 import g,pygame,utils,sys,buttons,jum,load_save
-try:
-    import gtk
-except:
-    pass
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 class Jumble:
 
@@ -60,7 +59,7 @@ class Jumble:
         while flushing:
             flushing=False
             if self.journal:
-                while gtk.events_pending(): gtk.main_iteration()
+                while Gtk.events_pending(): Gtk.main_iteration()
             for event in pygame.event.get(): flushing=True
 
     def run(self):
@@ -78,7 +77,7 @@ class Jumble:
         while going:
             if self.journal:
                 # Pump GTK messages.
-                while gtk.events_pending(): gtk.main_iteration()
+                while Gtk.events_pending(): Gtk.main_iteration()
 
             # Pump PyGame messages.
             for event in pygame.event.get():
