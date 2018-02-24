e274a551f7c57b3408df8138c310e58dc858db67
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2018-02-24 15:05:15 +0530

    Update sugargame to v1.2

--- Jumble.py
+++ Jumble.py
@@ -63,6 +63,12 @@ class Jumble:
             for event in pygame.event.get(): flushing=True
 
     def run(self):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                return
+            elif event.type == pygame.VIDEORESIZE:
+                pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                break
         g.init()
         if not self.journal: utils.load()
         load_save.retrieve()
